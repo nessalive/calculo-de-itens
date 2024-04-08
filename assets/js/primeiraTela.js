7d353383fb472f3c05e64b3419f1c79beefac407
Ana Carolina <carol00_10@hotmail.com>
2024-04-08 17:07:21 -0300

    update and upgrade

---
+++ assets/js/primeiraTela.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+ const form = document.getElementById('cadastroForm');
+
+ form.addEventListener('submit', function(event) {
+     // Captura os valores dos campos
+     const nome = document.getElementById('nome').value.trim();
+     const email = document.getElementById('email').value.trim();
+     const senha = document.getElementById('senha').value.trim();
+
+     // Validação dos campos
+     if (nome === '') {
+         alert('Por favor, insira seu nome.');
+         return;
+     }
+
+     if (!validarEmail(email)) {
+         alert('Por favor, insira um e-mail válido.');
+         return;
+     }
+
+     if (senha === '') {
+         alert('Por favor, insira sua senha.');
+         return;
+     }
+
+     // Salva os dados no localStorage
+     const dadosUsuario = {
+         nome: nome,
+         email: email,
+         senha: senha
+     };
+
+     localStorage.setItem('dadosUsuario', JSON.stringify(dadosUsuario));
+ });
+ window.location.href = '/segundaTela.html';
+});
+
+// Função para validar o formato do e-mail
+function validarEmail(email) {
+ const re = /\S+@\S+\.\S+/;
+ return re.test(email);
+}
